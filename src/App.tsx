1fd2d6552a90fd08f07ae794076602d8e9bfb44b
Alex Gabriel <bgabriel@etsy.com>
2021-11-05 00:40:44 -0600

    Clean imports to deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
-import CounterPage from "./pages/counter";
-import TodosPage from "./pages/todos";
-import AddTodoPage from "./pages/todos/Add";
 import ShowPokemonPage from "./pages/pokemon/Show";
 import Navigation from "./components/navigation";
 import ListPokemonPage from "./pages/pokemon/List";
